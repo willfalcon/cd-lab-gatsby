8effc0c3c7db6b48de500d7a7124027591e42f6b
Will Hawks <willkhawks@gmail.com>
2020-03-02 10:39:52 -0600

    think i fixed the zindexing nightmare

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import { rgba } from 'polished';
+import Img from 'gatsby-image';
 
 import MenuToggle from './MenuToggle';
 import SocialList from '../SocialList';
@@ -10,20 +13,73 @@ import { media } from '../theme';
 import SiteLogo from './SiteLogo';
 
 const Header = () => {
+  const images = useStaticQuery(graphql`
+    {
+      homeLogo: file(relativePath: { eq: "cd-type-padded.png" }) {
+        id
+        childImageSharp {
+          fixed(width: 387) {
+            base64
+            width
+            height
+            src
+            srcSet
+            srcWebp
+            srcSetWebp
+          }
+        }
+      }
+    }
+  `);
   const { menuOpen, home, expandedTopic } = useSiteContext();
   return (
-    <StyledHeader
-      topicsOpen={expandedTopic}
-      menuOpen={menuOpen}
-      className="header"
-    >
-      <SiteLogo home={home} />
-      <MenuToggle />
-      <SocialList />
-      <Nav />
-    </StyledHeader>
+    <>
+      <HomeLogoWrap to="/" className="home-wrap">
+        <Img
+          fixed={images.homeLogo.childImageSharp.fixed}
+          alt="Creative Distillery"
+          className="home-logo"
+          fadeIn={false}
+        />
+      </HomeLogoWrap>
+      <StyledHeader
+        topicsOpen={expandedTopic}
+        menuOpen={menuOpen}
+        className="header"
+      >
+        <SiteLogo home={home} />
+        <MenuToggle />
+        <SocialList />
+        <Nav />
+      </StyledHeader>
+    </>
   );
 };
+
+const HomeLogoWrap = styled(Link)`
+  background: white;
+  display: none;
+  cursor: pointer;
+  ${media.break`
+    flex-grow: 1;
+    line-height: 1;
+    /* z-index: 6; */
+    display: block;
+    height: 95px;
+    width: calc(100vw * .4 - 75px);
+    padding-bottom: 2rem;
+    position: absolute;
+    top: 0;
+    left: ${75 + 10}px;
+    background: ${({ theme }) =>
+      `linear-gradient(
+        ${rgba(theme.offWhite, 1)}, 
+        ${theme.offWhite} 35%,
+        ${theme.offWhite} 60%,
+        ${rgba(theme.offWhite, 0)}
+      )`};
+  `}
+`;
 
 const StyledHeader = styled.header`
   display: flex;
--- src/components/Header/SiteLogo.js
+++ src/components/Header/SiteLogo.js
@@ -1,81 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { rgba } from 'polished';
-import { Link, useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 
 import { media } from '../theme';
 import Square from '../../images/cd-new-square.png';
 import Logo from '../../images/cd-logo-h.png';
 
 const SiteLogo = ({ home }) => {
-  const images = useStaticQuery(graphql`
-    {
-      homeLogo: file(relativePath: { eq: "cd-type-padded.png" }) {
-        id
-        childImageSharp {
-          fixed(width: 387) {
-            base64
-            width
-            height
-            src
-            srcSet
-            srcWebp
-            srcSetWebp
-          }
-        }
-      }
-    }
-  `);
-
   return (
-    <>
-      {/* {home && ( */}
-      <HomeLogoWrap to="/" className="home-wrap">
-        <Img
-          fixed={images.homeLogo.childImageSharp.fixed}
-          alt="Creative Distillery"
-          className="home-logo"
-          fadeIn={false}
-        />
-      </HomeLogoWrap>
-      {/* )} */}
-      <StyledLogoLink to="/" className="site-logo">
-        <SquareLogo
-          className="square"
-          src={Square}
-          alt="Creative Distillery Icon"
-        />
-        <FullLogo className="full" src={Logo} alt="Creative Distillery" />
-      </StyledLogoLink>
-    </>
+    <StyledLogoLink to="/" className="site-logo">
+      <SquareLogo
+        className="square"
+        src={Square}
+        alt="Creative Distillery Icon"
+      />
+      <FullLogo className="full" src={Logo} alt="Creative Distillery" />
+    </StyledLogoLink>
   );
 };
-
-const HomeLogoWrap = styled(Link)`
-  background: white;
-  display: none;
-  cursor: pointer;
-  ${media.break`
-    flex-grow: 1;
-    line-height: 1;
-    /* z-index: 6; */
-    display: block;
-    height: 95px;
-    width: calc(100vw * .4 - 75px);
-    padding-bottom: 2rem;
-    position: absolute;
-    top: 0;
-    left: ${75 + 10}px;
-    background: ${({ theme }) =>
-      `linear-gradient(
-        ${rgba(theme.offWhite, 1)}, 
-        ${theme.offWhite} 35%,
-        ${theme.offWhite} 60%,
-        ${rgba(theme.offWhite, 0)}
-      )`};
-  `}
-`;
 
 const StyledLogoLink = styled(Link)`
   flex-grow: 1;
