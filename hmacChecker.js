15bbb27c7310dc721b434ed7cd024cfa5b5883aa
Will Hawks <willkhawks@gmail.com>
2020-03-03 10:17:47 -0600

    remove old deploy stuff

--- hmacChecker.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-module.exports = function(requestHmac, body, secret) {
-  const generatedHmac = crypto
-    .createHmac('sha1', secret)
-    .update(JSON.stringify(body))
-    .digest('hex');
-  return generatedHmac === requestHmac;
-};
--- scripts/gatsby-build.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/willfalcon/Sites/cd-lab.dev/gatsby
-git pull
-npm install
-npm run build
