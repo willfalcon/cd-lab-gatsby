6047402151b1ef804d5a6ae31b46f4027967ae83
Will Hawks <willkhawks@gmail.com>
2020-03-10 14:16:02 -0500

    add video serializer

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import BlockContent from '@sanity/block-content-to-react';
 import classNames from 'classnames';
+import ReactPlayer from 'react-player';
+
+const serializers = {
+  types: {
+    video: ({node}) => {
+      const { url } = node;
+      return <ReactPlayer url={url} controls />
+    }
+  }
+}
 
 const Content = ({ children, className }) => {
   return (
     <div className={classNames('block-content', className)}>
-      <BlockContent blocks={children} projectId="sgba0i04" dataset="dev" />
+      <BlockContent blocks={children} projectId="sgba0i04" dataset="dev" serializers={serializers} />
     </div>
   );
 };
